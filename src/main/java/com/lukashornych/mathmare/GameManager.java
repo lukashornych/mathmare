714316ea2e6d400ecb33eb66d17af5bd9f0e6712
Lukáš Hornych <lukashornych@protomail.com>
2021-04-29 18:15:11 +0200

    refactored main app

--- src/main/java/com/lukashornych/mathmare/GameManager.java
+++ src/main/java/com/lukashornych/mathmare/GameManager.java
@@ -3,8 +3,10 @@ package com.lukashornych.mathmare;
 import com.lukashornych.mathmare.scene.SceneManager;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
-import lombok.NonNull;
 import lombok.ToString;
+
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
 
 /**
  * Manager of entire game instance. It manages flow of the game using underlying managers and should be also
@@ -24,12 +26,8 @@ public class GameManager {
 
     /**
      * Creates new game manager for current GLFW window. This manager should only one in game instance.
-     *
-     * @param window
      */
-    public GameManager(@NonNull Window window) {
-        this.window = window;
-
+    public GameManager() {
         this.inputManager = new InputManager(this);
         this.sceneManager = new SceneManager(this);
     }
@@ -38,17 +36,42 @@ public class GameManager {
      * Initialize this manager as well as all underlying managers to usable state.
      */
     public void init() {
+        window = new Window(1024, 576);
+        window.init();
+
         inputManager.init();
         sceneManager.init();
     }
 
     /**
      * Updates game
-     *
-     * @param dt delta time from previous render
      */
-    public void update(float dt) {
-        inputManager.update();
-        sceneManager.update(dt);
+    public void run() {
+        long prevTime = System.currentTimeMillis();
+        float deltaTime = -1.0f;
+
+        while (!glfwWindowShouldClose(window.getId())) {
+            glViewport(0, 0, window.getWidth(), window.getHeight());
+
+            glfwPollEvents();
+
+            glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+            if (deltaTime > 0) {
+                inputManager.update();
+                sceneManager.update(deltaTime);
+            }
+
+            glfwSwapBuffers(window.getId());
+
+            final long currentTime = System.currentTimeMillis();
+            deltaTime = (currentTime - prevTime) / 1000.0f;
+            prevTime = currentTime;
+        }
+    }
+
+    public void destroy() {
+        window.destroy();
     }
 }
--- src/main/java/com/lukashornych/mathmare/Mathmare.java
+++ src/main/java/com/lukashornych/mathmare/Mathmare.java
@@ -1,8 +1,5 @@
 package com.lukashornych.mathmare;
 
-import static org.lwjgl.glfw.GLFW.*;
-import static org.lwjgl.opengl.GL11.*;
-
 /**
  * Application which start the whole game.
  *
@@ -10,47 +7,15 @@ import static org.lwjgl.opengl.GL11.*;
  */
 public class Mathmare {
 
-    private Window window;
-    private GameManager gameManager;
 
     public static void main(String... args) {
         new Mathmare().run();
     }
 
     public void run() {
-        window = new Window(1024, 576);
-        window.init();
-
-        gameManager = new GameManager(window);
+        final GameManager gameManager = new GameManager();
         gameManager.init();
-
-        loop();
-
-        window.destroy();
-    }
-
-    // todo refactor? to gamemanager?
-    private void loop() {
-        long prevTime = System.currentTimeMillis();
-        float deltaTime = -1.0f;
-
-        while (!glfwWindowShouldClose(window.getId())) {
-            glViewport(0, 0, window.getWidth(), window.getHeight());
-
-            glfwPollEvents();
-
-            glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-            if (deltaTime > 0) {
-                gameManager.update(deltaTime);
-            }
-
-            glfwSwapBuffers(window.getId());
-
-            final long currentTime = System.currentTimeMillis();
-            deltaTime = (currentTime - prevTime) / 1000.0f;
-            prevTime = currentTime;
-        }
+        gameManager.run();
+        gameManager.destroy();
     }
 }
